6ce5c4cb7dbc114bed7ef67097836671f4930e1a
Kaio Pereira <155786048+kaiopereiraj@users.noreply.github.com>
2025-04-21 16:45:41 -0300

    Create index.js

---
+++ aula6/index.js
@@ -0,0 +1,20 @@
+let nome = 'João'; // String
+
+
+console.log(nome + ' nasceu em 1984.');
+console.log(`Em 2000 ${nome} conheceu Maria.`);
+console.log(`${nome} casou-se com Maria em 2012.`);
+console.log(`Maria teve 1 filho com ${nome} em 2015.`);
+console.log(`O filho de ${nome} se chama Eduardo.`);
+
+let nm = 'Bruxa';
+console.log(`Quem sempre tentava acabar com esse casal era a ${nm}`);
+
+
+// Não podemos criar variáveis com palavras reservadas
+// Não podemos criar uma variável que o nome comece com número
+// Não podem conter espaços ou traços
+// Variáveis são Case-sensitive... Letras maiúsculas e minúsculas fazem diferença
+// Não podemos redeclarar variáveis com let
+
+// Variáveis precisam ter nomes significativos ( Para você entender melhor o teu código )
