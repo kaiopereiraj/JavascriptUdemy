543ba2d4d6d0127c1d5cc00862a222e92672ba54
Kaio Pereira <155786048+kaiopereiraj@users.noreply.github.com>
2025-04-21 15:56:36 -0300

    Update comentarios.js

--- aula4/comentarios.js
+++ aula4/comentarios.js
@@ -1,3 +1,13 @@
 //Escrever um comentário...
 
-console.log('Olá Mundo!')
+console.log('Olá Mundo!'); //Aqui tem outro comentário
+console.log('Linha 1');
+/* 
+
+    Um bloco de comentário
+    Um bloco de comentário
+
+*/
+console.log('Linha 2'); 
+//Os comentários vão ser ignorados...
+console.log('Linha 3');
