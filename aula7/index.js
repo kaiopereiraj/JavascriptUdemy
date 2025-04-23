49ddb361d480ea8759a23f4811ff9a0a8cd5a37d
Kaio Pereira <155786048+kaiopereiraj@users.noreply.github.com>
2025-04-22 21:41:46 -0300

    Update index.js

--- aula7/index.js
+++ aula7/index.js
@@ -12,6 +12,8 @@ const primeiroNumero = 5;
 const segundoNumero = 10;
 const resultado = primeiroNumero * segundoNumero;
 const resultadoDuplicado = resultado * 2;
+let resultadoTriplicado = resultado * 3;
 console.log(resultado);
 console.log(resultadoDuplicado);
+console.log(resultadoTriplicado);
 
