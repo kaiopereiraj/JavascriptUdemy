ccbbedbdabd153ac36040b34e685c735f3c90264
Kaio Pereira <155786048+kaiopereiraj@users.noreply.github.com>
2025-04-22 21:27:48 -0300

    Update index.js

---
+++ aula7/index.js
@@ -0,0 +1,6 @@
+// Não podemos criar constantes com palavras reservadas
+// Não podemos criar uma constantes que o nome comece com número
+// Não podem conter espaços ou traços
+// constantes são Case-sensitive... Letras maiúsculas e minúsculas fazem diferença
+// Não podemos modificar o valor de uma constante
+// Não utilize VAR, utilize let
