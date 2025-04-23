84afaa5ddd95995a84d952a49428ad70433762d0
Kaio Pereira <155786048+kaiopereiraj@users.noreply.github.com>
2025-04-22 21:39:29 -0300

    Update index.js

--- aula7/index.js
+++ aula7/index.js
@@ -7,3 +7,11 @@
 
 const nome = 'Kaio';
 console.log(nome);
+
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+const resultadoDuplicado = resultado * 2;
+console.log(resultado);
+console.log(resultadoDuplicado);
+
