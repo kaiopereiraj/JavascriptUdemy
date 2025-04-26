72e50cf173e08e3ed6e1c6e4b791dd005d9e0f88
Kaio Pereira <155786048+kaiopereiraj@users.noreply.github.com>
2025-04-25 23:03:19 -0300

    Update index.js

--- aula7/index.js
+++ aula7/index.js
@@ -10,7 +10,6 @@ const sobrenome = 'Pereira';
 console.log(nome);
 console.log(sobrenome);
 
-
 const primeiroNumero = 5;
 const segundoNumero = 10;
 const resultado = primeiroNumero * segundoNumero;
@@ -19,7 +18,6 @@ let resultadoTriplicado = resultado * 3;
 console.log(resultado);
 console.log(resultadoDuplicado);
 console.log(resultadoTriplicado);
-
 
 console.log(typeof primeiroNumero);
 
