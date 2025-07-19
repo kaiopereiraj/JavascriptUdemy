2a743824d4910472ef494272a5fc2473d07ee811
Kaio Pereira <155786048+kaiopereiraj@users.noreply.github.com>
2025-07-18 21:53:32 -0300

    Create index.js

---
+++ aula8/index.js
@@ -0,0 +1,20 @@
+/* 
+    Kaio  Jesus tem 22 anos, pesa 78 kg
+    tem 1.83 de altura e seu IMC é de 2..
+    Kaio Pereira nasceu em 2003
+
+*/
+
+const nome = 'Kaio';
+const sobrenome = 'Jesus';
+const idade = 22;
+const peso = 78;
+const alturaEmM = 1.83;
+let imc;
+let anoNascimento;
+
+imc = peso / (alturaEmM * alturaEmM);
+anoNascimento = 2025 - idade;
+
+console.log(`${nome} ${sobrenome} tem ${idade} anos, pesa ${peso} kg tem ${alturaEmM} de altura e seu IMC é de ${imc}`)
+
