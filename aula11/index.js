c5b848d9f8aceba60e3d2b3758acddff3cdcdbd9
Kaio Pereira <155786048+kaiopereiraj@users.noreply.github.com>
2025-07-20 20:53:25 -0300

    Update index.js

---
+++ aula11/index.js
@@ -0,0 +1,13 @@
+/* 
+    Operados aritméticos
+     +  Adição / Concatenação
+     -  Subtração
+     /  Divisão
+     *  Multiplicação
+
+*/
+
+const num1 = 5;
+const num2 = 10;
+console.log(num1 + num2);
+
