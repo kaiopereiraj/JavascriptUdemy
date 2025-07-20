20764681ff95279526d197c2220e7eaa194e46a1
Kaio Pereira <155786048+kaiopereiraj@users.noreply.github.com>
2025-07-20 20:38:17 -0300

    Update index.js

--- aula10/index.js
+++ aula10/index.js
@@ -1,3 +1,12 @@
 const nome = 'Kaio'; //string
 const nome1 = `Kaio`; //string
 const nome2 = "Kaio" //string
+
+const num1 = 10; //number
+const num2 = 10.5; //number
+
+let nomeALuno; //undefined = não aponta para local nenhum na memória
+
+const sobrenomeAluno = null; //nulo -> não aponta para local nenhum na memória
+
+const aprovado = true; // booelean = true or false.
