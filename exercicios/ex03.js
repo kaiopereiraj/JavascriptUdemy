6a0396f32bc2d867527f037fa2a4be40490f2f33
Kaio Pereira <155786048+kaiopereiraj@users.noreply.github.com>
2025-07-23 21:44:26 -0300

    Update ex03.js

--- exercicios/ex03.js
+++ exercicios/ex03.js
@@ -2,10 +2,16 @@ let varA = 'A';
 let varB = 'B';
 let varC = 'C';
 
-const varATemp = varA
-varA = varB;
-varB = varC;
-varC = varATemp;
+    /* 
+    
+    const varATemp = varA
+    varA = varB;
+    varB = varC;
+    varC = varATemp;
+    
+    */
+
+[varA, varB, varC] = [varB, varC, varA];
 
 
 console.log(varA, varB, varC);
