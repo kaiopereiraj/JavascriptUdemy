669098c3c03f7d0077af70111bd4e68e29bc8ce5
Kaio Pereira <155786048+kaiopereiraj@users.noreply.github.com>
2025-08-03 16:11:09 -0300

    Update index.js

--- aula14/index.js
+++ aula14/index.js
@@ -1,5 +1,16 @@
-let num1 = 15;
-let num2 = 2.8;
+// IEEE 754-2008
+
+let num1 = 0.7;
+let num2 = 0.1;
+
+num1 += num2; //0.8
+num1 += num2; //0.9
+num1 += num2; //1.0
+
+num1 = parseFloat(num1.toFixed(2));
+
+console.log(num1)
+console.log(Number.isInteger(num1))
 
 //console.log(num1.toString() + num2);
 //console.log(typeof num1)
@@ -7,4 +18,7 @@ let num2 = 2.8;
 //console.log(num1.toString(2)); Representação Binária
 
 //console.log(num1.toFixed(2));
-console.log(Number.isInteger(num1));
+//console.log(Number.isInteger(num1));
+//let temp = num1 + '8';
+//console.log(Number.isNaN(temp));
+//console.log(temp)
