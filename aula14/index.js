c4e0b131902b3a3a0a148526b8d0852776376c71
Kaio Pereira <155786048+kaiopereiraj@users.noreply.github.com>
2025-08-03 16:15:17 -0300

    Update index.js

--- aula14/index.js
+++ aula14/index.js
@@ -3,11 +3,11 @@
 let num1 = 0.7;
 let num2 = 0.1;
 
-num1 += num2; //0.8
-num1 += num2; //0.9
-num1 += num2; //1.0
+num1 = ((num1 * 100) + (num2 * 100)) / 100; //0.8
+num1 = ((num1 * 100) + (num2 * 100)) / 100; //0.8
+num1 = ((num1 * 100) + (num2 * 100)) / 100; //0.8
 
-num1 = parseFloat(num1.toFixed(2));
+//num1 = parseFloat(num1.toFixed(2));
 
 console.log(num1)
 console.log(Number.isInteger(num1))
