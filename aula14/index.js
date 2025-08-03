752b2a40676c11610c8d4bc3b1dad660512000fe
Kaio Pereira <155786048+kaiopereiraj@users.noreply.github.com>
2025-08-03 15:57:37 -0300

    Create index.js

---
+++ aula14/index.js
@@ -0,0 +1,9 @@
+let num1 = 15;
+let num2 = 2.8;
+
+//console.log(num1.toString() + num2);
+//console.log(typeof num1)
+
+//console.log(num1.toString(2)); Representação Binária
+
+//console.log(num1.toFixed(2));
