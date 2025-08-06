3bfaa8d2182546cee807562edcc8de9a834ef7eb
Kaio Pereira <155786048+kaiopereiraj@users.noreply.github.com>
2025-08-05 22:52:10 -0300

    Update index.js

--- aula15/index.js
+++ aula15/index.js
@@ -1,4 +1,6 @@
-let num1 = 9.84875;
+let num1 = 81;
+
+console.log(num1 ** 0.5);
 
 //let num2 = Math.floor(num1); Irá arrendondar para 9
 //let num2 = Math.ceil(num1); Irá arrendondar para 10
@@ -6,3 +8,7 @@ let num1 = 9.84875;
 
 //console.log(Math.max(1, 2, 3, 48, -7, 155)); Irá encontrar o maior número dessa sequencia
 //console.log(Math.min(1, 2, 3, 48, -7, 155)); Irá encontrar o menor número dessa sequencia
+
+//const aleatorio = Math.round(Math.random() * (10 - 5) + 5);
+
+console.log();
