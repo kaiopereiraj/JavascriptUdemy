ec4c6b073aa91892259dd97fd014ef8650f02380
Kaio Pereira <155786048+kaiopereiraj@users.noreply.github.com>
2025-08-12 23:07:54 -0300

    Update index.js

--- aula15/index.js
+++ aula15/index.js
@@ -4,6 +4,8 @@ let num1 = 9.48618;
 //let num2 = Math.ceil(num1); Irá arrendondar para 10
 //let num2 = Math.round(num1);
 
+
+
 //console.log(Math.max(1, 2, 3, 48, -7, 155)); Irá encontrar o maior número dessa sequencia
 //console.log(Math.min(1, 2, 3, 48, -7, 155)); Irá encontrar o menor número dessa sequencia
 
